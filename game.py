e36ff4117a3ff579b68273e659b23de0aba438f8
yU-kiki <yuki521ike@gmail.com>
2023-01-17 02:11:23 +0900

    3Dマップの描画

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import math
 PI = 3.1415926535
 P2 = PI / 2
 P3 = 3 * PI / 2
+DR = 0.0174533 # one degree to radius
 class App:
     def __init__(self):
         pyxel.init(1024,512)
@@ -25,52 +26,65 @@ class App:
         self.pdx = math.cos(self.pa) * 5
         self.pdy = math.sin(self.pa) * 5
 
-        self.ra = self.pa
         self.rx = 0
         self.ry = 0
 
+        self.rayList = []
+        for _ in range(60):
+            self.rayList.append(None)
+
         pyxel.run(self.update, self.draw)
+    
+    def dist(self, x1, y1, x2, y2, ang):
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 
     def update(self):
-        if pyxel.btn(pyxel.KEY_A):
+        if pyxel.btn(pyxel.KEY_LEFT):
             self.pa -= 0.1
             if self.pa < 0:
                 self.pa += 2 * PI
             self.pdx = math.cos(self.pa) * 5
             self.pdy = math.sin(self.pa) * 5
-        if pyxel.btn(pyxel.KEY_D):
+        if pyxel.btn(pyxel.KEY_RIGHT):
             self.pa += 0.1
             if self.pa > 2 * PI:
                 self.pa -= 2 * PI
             self.pdx = math.cos(self.pa) * 5
             self.pdy = math.sin(self.pa) * 5
-        if pyxel.btn(pyxel.KEY_W):
+        if pyxel.btn(pyxel.KEY_UP):
             self.px += self.pdx
             self.py += self.pdy
-        if pyxel.btn(pyxel.KEY_S):
+        if pyxel.btn(pyxel.KEY_DOWN):
             self.px -= self.pdx
             self.py -= self.pdy
 
         self.update_rays()
 
     def update_rays(self):
-        r = 0
-        self.ra = self.pa
-        while r < 1:
+        ra = self.pa - DR * 30
+        if ra < 0:
+            ra += 2 * PI
+        if ra > 2 * PI:
+            ra -= 2 * PI 
+        for i in range(60):
             # check Horizontal lines
             dof = 0
-            aTan = -1 / math.tan(self.ra)
-            if self.ra > PI:
+            disH = 1000000
+            hx = self.px
+            hy = self.py
+            aTan = -1 / math.tan(ra)
+            if ra > PI:
                 self.ry = ((int(self.py) >> 6) << 6) - 0.0001
                 self.rx = (self.py - self.ry) * aTan + self.px
                 yo = -64
                 xo = -yo  * aTan
-            if self.ra < PI:
+            if ra < PI:
                 self.ry = ((int(self.py) >> 6) << 6) + 64
                 self.rx = (self.py - self.ry) * aTan + self.px
                 yo = 64
                 xo = -yo  * aTan
-            if self.ra == 0 or self.ra == PI:
+            if ra == 0 or ra == PI:
                 self.rx = self.px
                 self.ry = self.py
                 dof = 8
@@ -78,27 +92,32 @@ class App:
                 mx = int(self.rx) >> 6
                 my = int(self.ry) >> 6
                 if 0 <= mx < self.mapX and 0 <= my < self.mapY and self.worldMap[mx][my] == 1:
+                    hx = self.rx
+                    hy = self.ry
+                    disH = self.dist(self.px, self.py, hx, hy, ra)
                     dof = 8
                 else:
                     self.rx += xo
                     self.ry += yo
                     dof += 1
-            # pyxel.line(self.px, self.py, self.rx, self.ry, 9)
 
             # check Vertical lines
             dof = 0
-            nTan = -math.tan(self.ra)
-            if self.ra > P2 and self.ra < P3:
+            disV = 1000000
+            vx = self.px
+            vy = self.py
+            nTan = -math.tan(ra)
+            if ra > P2 and ra < P3:
                 self.rx = ((int(self.px) >> 6) << 6) - 0.0001
                 self.ry = (self.px - self.rx) * nTan + self.py
                 xo = -64
                 yo = -xo  * nTan
-            if self.ra < P2 or self.ra > P3:
+            if ra < P2 or ra > P3:
                 self.rx = ((int(self.px) >> 6) << 6) + 64
                 self.ry = (self.px - self.rx) * nTan + self.py
                 xo = 64
                 yo = -xo  * nTan
-            if self.ra == 0 or self.ra == PI:
+            if ra == 0 or ra == PI:
                 self.rx = self.px
                 self.ry = self.py
                 dof = 8
@@ -106,20 +125,59 @@ class App:
                 mx = int(self.rx) >> 6
                 my = int(self.ry) >> 6
                 if 0 <= mx < self.mapX and 0 <= my < self.mapY and self.worldMap[mx][my] == 1:
+                    vx = self.rx
+                    vy = self.ry
+                    disV = self.dist(self.px, self.py, vx, vy, ra)
                     dof = 8
                 else:
                     self.rx += xo
                     self.ry += yo
                     dof += 1
-            # pyxel.line(self.px, self.py, self.rx, self.ry, 9)
-            r += 1
+
+            disT = 0
+            hit = 0
+            if disV < disH:
+                self.rx = vx
+                self.ry = vy
+                disT = disV
+                hit = 0
+            if disH < disV:
+                self.rx = hx
+                self.ry = hy
+                disT = disH
+                hit = 1
+
+            ca = self.pa - ra
+            if ca < 0:
+                ca += 2 * PI
+            if ca > 2 * PI:
+                ca -= 2 * PI
+            disT = disT * math.cos(ca)
+            lineH = (self.mapS * 320) / disT
+            if lineH > 320:
+                lineH = 320
+            lineO = 160 - lineH / 2
+            
+            self.rayList[i] = [self.rx, self.ry, lineH, lineO, hit]
+
+            ra += DR
+            if ra < 0:
+                ra += 2 * PI
+            if ra > 2 * PI:
+                ra -= 2 * PI
 
     def draw(self):
         pyxel.cls(13)
         self.draw_2dMap()
         pyxel.circ(self.px, self.py, 5, 10)
         pyxel.line(self.px, self.py, self.px + self.pdx * 5, self.py + self.pdy * 5, 10)
-        pyxel.line(self.px, self.py, self.rx, self.ry, 11)
+        for i, v in enumerate(self.rayList):
+            pyxel.line(self.px, self.py, v[0], v[1], 11)
+            if v[4] == 0:
+                col = 3
+            elif v[4] == 1:
+                col = 11
+            pyxel.rect(i * 8 + 530, v[3], 8, v[2], col)
 
     def draw_2dMap(self):
         for y in range(self.mapY):
